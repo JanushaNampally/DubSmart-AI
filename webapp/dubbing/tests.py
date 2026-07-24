4f791e0492c59e8d3eba04564e3a335a8f3709f6
Janusha <2310080064@klh.edu.in>
2026-07-24 13:22:42 +0530

    Add comments to tests.py
    Added comments to explain the test file and the dubbing process.

--- webapp/dubbing/tests.py
+++ webapp/dubbing/tests.py
@@ -1,3 +1,5 @@
 from django.test import TestCase
 
 # Create your tests here.
+#create your tests here.
+#the dubsmart ai is a 6 stage process which automatically dubs from hindi audio to telugu audio manually
